8612b5c20435020e4a0b4b0ed9a61bc872c02a8b
Bernardo Silva <benny.stuff@gmail.com>
2013-08-31 23:08:20 +0100

    first commit

---
+++ application/views/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CityFinder</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/main_styles.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+
+    <link href='http://fonts.googleapis.com/css?family=Copse' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+  </head>
+  <body>
+  <div class="app-wrapper span6">
+  	<header>
+  		<img src="img/logo.png">
+	  </header>
+	  <div class="app-breadcrumb">
+	  	<ul class="breadcrumb-list">
+			  <li ><a href="#">Home</a> <span class="divider">></span></li>
+			  <li><a href="#">Library</a> <span class="divider">></span></li>
+			  <li class="active">Data</li>
+			</ul>
+	  </div>
+	  <div class="app-container">
+	  	<ul class="listview">
+	  		<li class="">
+	  			<a href="">Country 1</a>
+	  		</li>
+	  		<li class="">
+	  			<a href="">Country 2</a>
+	  		</li>
+	  		<li class="">
+	  			<a href="">Country 3</a>
+	  		</li>
+	  	</ul>
+	  </div>
+	  	
+	  	
+  </div>
+  
